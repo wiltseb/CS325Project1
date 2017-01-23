510bcdff285646885657269a6773685f6028c8eb
brian wiltse <wiltseb@oregonstate.edu>
2017-01-22 23:25:15 -0800

    Added enumerative version

--- project1Template.py
+++ project1Template.py
@@ -1,6 +1,8 @@
 '''
 Reads data from input file
 Returns list of list of ints
+Parameters:
+filename - string name of file to open
 '''
 def getInputData(filename):
     data = []
@@ -11,9 +13,43 @@ def getInputData(filename):
             data.append(line)
     return data
 
+'''
+writes data to file in specified format
+Parameters:
+outFile - file object in write mode
+A - full array
+startPos - starting position of subArray
+endPos - end position of subArray
+total - sum of maximum subArray
+'''
+def writeData(outFile, A, startPos, endPos, total):
+    outFile.write(" ".join(str(x) for x in A) + '\n')
 
-
+    outFile.write(" ".join(str(A[x]) for x in range(startPos, endPos+1)) + '\n')
+    outFile.write(str(total) + '\n\n')
+    
+'''
+enumerative version of MaxSubArray
+'''
+def Enum_Max_Subarray(A, outFile):
+    maximum = A[0]
+    subArrayStart = 0
+    subArrayEnd = 0
+    currentTotal = 0
+    for i in range(len(A)):
+        currentTotal = A[i]
+        for j in range(i+1, len(A)):
+            currentTotal += A[j]
+            if currentTotal > maximum:
+                maximum = currentTotal
+                subArrayStart = i
+                subArrayEnd = j
+    writeData(outFile, A, subArrayStart, subArrayEnd, maximum)
+    
+    
 data = getInputData("MSS_TestProblems.txt")
-print(data)
-
+outFile = open("out.txt", 'w')
+for i in range(len(data)):
+    Enum_Max_Subarray(data[i], outFile)
+outFile.close()
 

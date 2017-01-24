85c6739acefaa46ace640b994ae5d43251443b97
brian wiltse <wiltseb@oregonstate.edu>
2017-01-24 03:18:49 -0800

    Enum and better enum functions working on test files

--- project1Template.py
+++ project1Template.py
@@ -24,14 +24,40 @@ total - sum of maximum subArray
 '''
 def writeData(outFile, A, startPos, endPos, total):
     outFile.write(" ".join(str(x) for x in A) + '\n')
-
     outFile.write(" ".join(str(A[x]) for x in range(startPos, endPos+1)) + '\n')
     outFile.write(str(total) + '\n\n')
     
 '''
-enumerative version of MaxSubArray
+enumerative version of Max_Subarray
+computes A[i] + A[i+1] + ... + A[j-1] + A[j] each time
+Parameters:
+A - an array of ints
+outFile - file object in write mode
 '''
 def Enum_Max_Subarray(A, outFile):
+    maximum = A[0]
+    subArrayStart = 0
+    subArrayEnd = 0
+    currentTotal = 0
+    for i in range(len(A)):
+        for j in range(i+1, len(A)):
+            currentTotal = 0
+            for k in range (i, j+1):
+                currentTotal += A[k]
+                if currentTotal > maximum:
+                    maximum = currentTotal
+                    subArrayStart = i
+                    subArrayEnd = j
+    writeData(outFile, A, subArrayStart, subArrayEnd, maximum)
+
+'''
+Better enumerative version of Max_Subarray
+stores prior computations of A[i] + A[i+1] + ... + A[j-1] 
+Parameters:
+A - an array of ints
+outFile - file object in write mode
+'''
+def Better_Enum_Max_Subarray(A, outFile):
     maximum = A[0]
     subArrayStart = 0
     subArrayEnd = 0
@@ -45,11 +71,12 @@ def Enum_Max_Subarray(A, outFile):
                 subArrayStart = i
                 subArrayEnd = j
     writeData(outFile, A, subArrayStart, subArrayEnd, maximum)
+
     
     
 data = getInputData("MSS_TestProblems.txt")
-outFile = open("out.txt", 'w')
+outFile = open("out.txt", 'w') #open outFile here so it overwrites existing file only once
 for i in range(len(data)):
-    Enum_Max_Subarray(data[i], outFile)
+    Enum_Max_Subarray(data[i], outFile)  # call whichever function here
 outFile.close()
 

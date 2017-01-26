c23c546af94deedc41677e2d529997071bb01580
brian wiltse <wiltseb@oregonstate.edu>
2017-01-25 19:56:42 -0800

    fixed some documentation

--- project1Template.py
+++ project1Template.py
@@ -34,7 +34,7 @@ enumerative version of Max_Subarray
 computes A[i] + A[i+1] + ... + A[j-1] + A[j] each time
 Parameters:
 A - an array of ints
-outFile - file object in write mode
+Returns - array consisting of original array, startPositon of subarray, endPositon of subArray, and maximum subarray sum
 '''
 def Enum_Max_Subarray(A):
     maximum = A[0]
@@ -58,6 +58,8 @@ stores prior computations of A[i] + A[i+1] + ... + A[j-1]
 Parameters:
 A - an array of ints
 outFile - file object in write mode
+Returns - array consisting of original array, startPositon of subarray, endPositon of subArray, and maximum subarray sum
+
 '''
 def Better_Enum_Max_Subarray(A):
     maximum = A[0]
@@ -79,6 +81,7 @@ get time data from function with various input sizes
 parameters:
 functionToCall: function of algorithm you want to test
 inputSizes: list of input sizes
+Returns - A list of time data for each inputSize
 '''
 def timeFunction(functionToCall, inputSizes):
     testLists = []
@@ -104,12 +107,16 @@ def getExperimentalData(function, inputSizes):
     timeList = timeFunction(function, inputSizes)
     print(str(function))
     for x in range(len(timeList)):
-        print("n = " + str(enumInputSizes[x]) + ": " + str(timeList[x]))
+        print("n = " + str(inputSizes[x]) + ": " + str(timeList[x]))
 
 '''
 creates output file
 parameters:
+
 function - function of algorithm you want to call
+this function should take in a single list of integers, and return a list in the form:
+[Full array, Start of subarray, end of subarray, sum of subarray]
+
 inFilename - string of filename for input
 outFilename - string of filename for output
 '''
@@ -123,18 +130,30 @@ def createOutputFile(function, inFilename, outFilename):
         writeData(outFile, params[0], params[1], params[2], params[3])
     outFile.close()
 
-'''    
+
+#-----Similar calls should work for your function as long as the function takes in a single list of integers, and returns a list in the form:
+#[Full array, Start of subarray, end of subarray, sum of subarray]
+    
+
+'''
+These one's should be good for actual experimental data, but they take awhile:
+'''
 enumInputSizes = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550]
 betterEnumInputSizes = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+
+
+
 '''
+#shorter tests to try your code on (shouldn't take forever)
 enumInputSizes = [50, 100, 150, 200, 250]
 betterEnumInputSizes = [100, 200, 300, 400, 500]
+'''
 
 #Gets experimental data for various n values for both enum functions
 getExperimentalData(Enum_Max_Subarray, enumInputSizes)
 getExperimentalData(Better_Enum_Max_Subarray, betterEnumInputSizes)
 
-#takes in input file and writes results in specified file
+#takes in input file and writes results in specified file -- see note above about functions to pass in
 createOutputFile(Enum_Max_Subarray, 'MSS_TestProblems.txt', 'outEnum.txt')
 createOutputFile(Better_Enum_Max_Subarray, 'MSS_TestProblems.txt', 'outBE.txt')
 
